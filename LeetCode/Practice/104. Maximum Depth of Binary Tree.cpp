53b28ffffec5c94e0168bc755774c09635be88c6
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-03-25 01:52:45 +0530

    25-03-23

--- LeetCode/Practice/104. Maximum Depth of Binary Tree.cpp
+++ LeetCode/Practice/104. Maximum Depth of Binary Tree.cpp
@@ -12,18 +12,10 @@
 class Solution
 {
 public:
-    int minDepth(TreeNode *root)
-    {
-        ios_base::sync_with_stdio(false);
-        if (!root)
-            return 0;
-        if (root->left && root->right)
-        {
-            return (1 + min(minDepth(root->left), minDepth(root->right)));
-        }
-        else
-        {
-            return (1 + max(minDepth(root->left), minDepth(root->right)));
-        }
-    }
+   int maxDepth(TreeNode *root)
+   {
+      if (!root)
+         return 0;
+      return 1 + max(maxDepth(root->left), maxDepth(root->right));
+   }
 };
