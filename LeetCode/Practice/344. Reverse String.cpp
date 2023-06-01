f5f9f5f40ca6348d2d67b2739537ac9e219787ba
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-06-01 23:11:16 +0530

    01-06-23

---
+++ LeetCode/Practice/344. Reverse String.cpp
@@ -0,0 +1,22 @@
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s) {
+//         int i=0, j=s.size()-1;
+//         while(i<=j){
+//             swap(s[i], s[j]);
+//             i++;
+//             j--;
+//         }
+//     }
+// };
+class Solution
+{
+public:
+   void reverseString(vector<char> &s)
+   {
+      for (int i = 0; i < s.size() / 2; i++)
+      {
+         swap(s[i], s[s.size() - i - 1]);
+      }
+   }
+};
