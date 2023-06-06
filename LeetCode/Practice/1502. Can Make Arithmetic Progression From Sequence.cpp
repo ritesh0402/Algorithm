0bafeb811fbcbf1e905c6a9a25649b4b2ee9e8d3
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-06-07 02:48:03 +0530

    07-06-23

--- LeetCode/Practice/1502. Can Make Arithmetic Progression From Sequence.cpp
+++ LeetCode/Practice/1502. Can Make Arithmetic Progression From Sequence.cpp
@@ -3,6 +3,7 @@ class Solution
 public:
    bool canMakeArithmeticProgression(vector<int> &arr)
    {
+      ios_base::sync_with_stdio(false);
       sort(arr.begin(), arr.end());
       int diff = arr[1] - arr[0];
       for (int i = 2; i < arr.size(); i++)
