cc0a5995b85f30b61b712b613121504abb0836c4
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-06-09 20:08:35 +0530

    09-06-23

---
+++ LeetCode/Practice/744. Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+   char nextGreatestLetter(vector<char> &letters, char target)
+   {
+      int l = 0;
+      int r = letters.size() - 1;
+      if (letters[r] <= target)
+         return letters[0];
+      while (l <= r)
+      {
+         int mid = l + (r - l) / 2;
+         if (letters[mid] > target)
+            r = mid - 1;
+         else
+            l = mid + 1;
+      }
+
+      return letters[l];
+   }
+};
+
+// class Solution {
+// public:
+//     char nextGreatestLetter(vector<char>& letters, char target) {
+//         int l = 0;
+//         int r = letters.size()-1;
+
+//         if (letters[r] <= target) {
+//             return letters[0];
+//         }
+//         while (l <= r) {
+//             int mid = l + (r - l) / 2;
+//             if (letters[mid] > target) {
+//                 r = mid-1;
+//             } else {
+//                 l = mid+1;
+//             }
+//         }
+//         return letters[l];
+//     }
+// };
