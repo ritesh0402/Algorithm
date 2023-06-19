49147342ac04b746a5c906b6eb28c4e17a7ff6db
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-06-20 04:49:42 +0530

    20-06-23

---
+++ LeetCode/Practice/46. Permutations.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+   vector<vector<int>> permute(vector<int> &nums)
+   {
+      vector<vector<int>> ans;
+      permutations(0, nums, ans);
+      return ans;
+   }
+
+   void permutations(int index, vector<int> &nums, vector<vector<int>> &ans)
+   {
+      if (index == nums.size())
+      {
+         ans.push_back(nums);
+         return;
+      }
+      for (int i = index; i < nums.size(); i++)
+      {
+         swap(nums[index], nums[i]);
+         permutations(index + 1, nums, ans);
+         swap(nums[index], nums[i]);
+      }
+   }
+};
