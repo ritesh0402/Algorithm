2fcdc5692d86d076ae19c1947e1e6adb1e8819eb
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-06-21 14:06:23 +0530

    21-06-23

--- LeetCode/Practice/1_Two_Sum.cpp
+++ LeetCode/Practice/1_Two_Sum.cpp
@@ -1,36 +1,17 @@
-// class Solution
-// {
-// public:
-//     vector<int> twoSum(vector<int> &nums, int target)
-//     {
-//         unordered_map<int, int> ump;
-//         for (int i = 0; i < nums.size(); i++)
-//         {
-//             if (ump.find(target - nums[i]) != ump.end())
-//             {
-//                 return {ump[target - nums[i]], i};
-//             }
-//             ump[nums[i]] = i;
-//         }
-//         return {};
-//     }
-// };
-
 class Solution
 {
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        int hm[target];
-        memset(hm, -1, sizeof(hm));
+        unordered_map<int, int> ump;
         for (int i = 0; i < nums.size(); i++)
         {
-            if (target - nums[i] > 0 && hm[target - nums[i]] != -1)
+            if (ump.find(target - nums[i]) != ump.end())
             {
-                return {i, hm[target - nums[i]]};
+                return {ump[target - nums[i]], i};
             }
-            hm[nums[i]] = i;
+            ump[nums[i]] = i;
         }
-        return {0, 0};
+        return {};
     }
 };
---
+++ LeetCode/Practice/39. Combination Sum.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+   vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+   {
+      vector<vector<int>> ans;
+      vector<int> seq;
+      subSeq(0, target, seq, candidates, ans);
+      return ans;
+   }
+   void subSeq(int index, int target, vector<int> &seq, vector<int> &candidates, vector<vector<int>> &ans)
+   {
+      if (index == candidates.size())
+      {
+         if (target == 0)
+         {
+            ans.push_back(seq);
+         }
+         return;
+      }
+      if (candidates[index] <= target)
+      {
+         seq.push_back(candidates[index]);
+         subSeq(index, target - candidates[index], seq, candidates, ans);
+         seq.pop_back();
+      }
+      subSeq(index + 1, target, seq, candidates, ans);
+   }
+};
