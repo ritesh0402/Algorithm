ee9c8e2973a795154a96a98f858f1f93621d1ed0
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-06-25 17:37:49 +0530

    25-06-23

---
+++ LeetCode/Practice/19. Remove Nth Node From End of List.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+   ListNode *removeNthFromEnd(ListNode *head, int n)
+   {
+      ListNode *newHead = new ListNode(0);
+      newHead->next = head;
+      ListNode *p = newHead;
+      ListNode *runner = newHead;
+
+      while (n > 0)
+      {
+         runner = runner->next;
+         n--;
+      }
+
+      while (runner->next != nullptr)
+      {
+         runner = runner->next;
+         p = p->next;
+      }
+
+      p->next = p->next->next;
+      return newHead->next;
+   }
+};
