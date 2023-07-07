e516ff7a1354d3a5dfd2c4240eed6b7ea54932b2
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-07-08 01:53:29 +0530

    08-07-23

---
+++ LeetCode/Practice/733. Flood Fill.cpp
@@ -0,0 +1,65 @@
+// class Solution {
+// public:
+//     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+//         if(image[sr][sc] == color) return image;
+//         int n = image.size();
+//         int m = image[0].size();
+//         int islands = 0;
+//         dfs(sr, sc, image[sr][sc], color, n, m, image);
+//         return image;
+//     }
+
+//     void dfs(int i, int j, int icolor,  int newColor, int n, int m, vector<vector<int>>& image){
+//         image[i][j] = newColor;
+//         //up
+//         if(i>0 && image[i-1][j] == icolor) dfs(i-1, j, icolor, newColor, n, m, image);
+//         //right
+//         if(j<m-1 && image[i][j+1] == icolor) dfs(i, j+1, icolor, newColor, n, m, image);
+//         //down
+//         if(i<n-1 && image[i+1][j] == icolor) dfs(i+1, j, icolor, newColor, n, m, image);
+//         //left
+//         if(j>0 && image[i][j-1] == icolor) dfs(i, j-1, icolor, newColor, n, m, image);
+//     }
+// };
+
+class Solution
+{
+public:
+   vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int newColor)
+   {
+      ios_base::sync_with_stdio(false);
+      cin.tie(nullptr);
+      cout.tie();
+
+      int ROW = image.size();
+      int COL = image[0].size();
+      int oldColor = image[sr][sc];
+      if (oldColor == newColor)
+         return image;
+
+      vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+      queue<pair<int, int>> q;
+      q.push({sr, sc});
+      image[sr][sc] = newColor;
+
+      while (!q.empty())
+      {
+         int cr = q.front().first;
+         int cc = q.front().second;
+         q.pop();
+
+         for (const auto &[dr, dc] : directions)
+         {
+            int nr = cr + dr;
+            int nc = cc + dc;
+            if (nr >= 0 && nr < ROW && nc >= 0 && nc < COL && image[nr][nc] == oldColor)
+            {
+               q.push({nr, nc});
+               image[nr][nc] = newColor;
+            }
+         }
+      }
+
+      return image;
+   }
+};
