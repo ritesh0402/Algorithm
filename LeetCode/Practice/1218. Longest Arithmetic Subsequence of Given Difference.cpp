94aae4c4cc73d7b839fa9ad429f6989cd672d32c
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-07-14 21:25:45 +0530

    14-07-23

---
+++ LeetCode/Practice/1218. Longest Arithmetic Subsequence of Given Difference.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+   int longestSubsequence(std::vector<int> &arr, int d)
+   {
+      unordered_map<int, int> mp;
+      int mx = 0;
+      for (int i : arr)
+      {
+         mp[i] = mp[i - d] + 1;
+         mx = std::max(mp[i], mx);
+      }
+      return mx;
+   }
+};
