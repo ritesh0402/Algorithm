07065127383be2af142188ed7ec947f926980ed7
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2023-07-25 07:06:19 +0530

    25-07-23

---
+++ LeetCode/Practice/210. Course Schedule II.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+   vector<int> findOrder(int n, vector<vector<int>> &prerequisites)
+   {
+      vector<int> adj[n];
+      vector<int> indegree(n, 0);
+      vector<int> ans;
+
+      for (auto x : prerequisites)
+      {
+         adj[x[1]].push_back(x[0]);
+         indegree[x[0]]++;
+      }
+
+      queue<int> q;
+      for (int i = 0; i < n; i++)
+      {
+         if (indegree[i] == 0)
+         {
+            q.push(i);
+         }
+      }
+
+      while (!q.empty())
+      {
+         auto t = q.front();
+         ans.push_back(t);
+         q.pop();
+
+         for (auto x : adj[t])
+         {
+            indegree[x]--;
+            if (indegree[x] == 0)
+            {
+               q.push(x);
+            }
+         }
+      }
+      if (ans.size() == n)
+         return ans;
+      return {};
+   }
+};
