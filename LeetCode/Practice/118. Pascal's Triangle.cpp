ed0d08d233207faf549004bf70c6d062e8e06715
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2024-03-09 19:02:43 +0530

    09-03-2024

--- LeetCode/Practice/118. Pascal's Triangle.cpp
+++ LeetCode/Practice/118. Pascal's Triangle.cpp
@@ -1,22 +1,45 @@
+// class Solution {
+// public:
+//     vector<vector<int>> generate(int numRows) {
+//         vector<vector<int>> result;
+//         vector<int> prevRow;
+
+//         for (int i = 0; i < numRows; i++) {
+//             vector<int> currentRow(i + 1, 1);
+
+//             for (int j = 1; j < i; j++) {
+//                 currentRow[j] = prevRow[j - 1] + prevRow[j];
+//             }
+
+//             result.push_back(currentRow);
+//             prevRow = currentRow;
+//         }
+
+//         return result;
+//     }
+// };
+
 class Solution
 {
 public:
    vector<vector<int>> generate(int numRows)
    {
       vector<vector<int>> pt;
-      for (int row = 1; row <= numRows; row++)
+      for (int row = 0; row < numRows; row++)
       {
          int ans = 1;
          pt.push_back(vector<int>());
-         pt[row - 1].push_back(ans);
-         for (int col = 2; col <= row; col++)
+         pt[row].push_back(ans);
+         for (int col = 1; col < row + 1; col++)
          {
             ans *= (row - col + 1);
-            ans /= (col - 1);
-            pt[row - 1].push_back(ans);
-            // cout << row << " " << col << endl;
+            ans /= (col);
+            pt[row].push_back(ans);
          }
       }
       return pt;
    }
 };
+
+// eg 5th row: 1 5 10 10 5 1
+// 1/1  5/1  5*4/1*2  5*4*3/1*2*3  5*4*3*2/1*2*3*4  5*4*3*2*1/1*2*3*4*5
--- LeetCode/Practice/2540. Minimum Common Value.cpp
+++ LeetCode/Practice/2540. Minimum Common Value.cpp
@@ -12,8 +12,36 @@ class Solution
 public:
    int getCommon(vector<int> &nums1, vector<int> &nums2)
    {
-      set<int> s;
       for (int i = 0, j = 0; i < nums1.size() && j < nums2.size();)
+      {
+         if (nums1[i] == nums2[j])
+         {
+            return nums1[i];
+         }
+         else if (nums1[i] < nums2[j])
+         {
+            i++;
+         }
+         else
+         {
+            j++;
+         }
+      }
+      return -1;
+   }
+};
+
+class Solution
+{
+public:
+   int getCommon(vector<int> &nums1, vector<int> &nums2)
+   {
+      ios::sync_with_stdio(0);
+      cin.tie(0);
+      cout.tie(0);
+      int i = 0, j = 0;
+      int n = nums1.size(), m = nums2.size();
+      while (i < n && j < m)
       {
          if (nums1[i] == nums2[j])
          {
