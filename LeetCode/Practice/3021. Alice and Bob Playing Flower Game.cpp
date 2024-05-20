64e3c2b74ae316626d5c11704ab29973908e9caa
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2024-05-20 23:09:29 +0530

    20-05-24

---
+++ LeetCode/Practice/3021. Alice and Bob Playing Flower Game.cpp
@@ -0,0 +1,9 @@
+class Solution
+{
+public:
+   long long flowerGame(int n, int m)
+   {
+      long long ans = n;
+      return (ans * m) / 2;
+   }
+};
--- LeetCode/Practice/605. Can Place Flowers.cpp
+++ LeetCode/Practice/605. Can Place Flowers.cpp
@@ -18,3 +18,23 @@ public:
       return n <= 0;
    }
 };
+
+class Solution
+{
+public:
+   bool canPlaceFlowers(vector<int> &flowerbed, int n)
+   {
+      for (int i = 0; i < flowerbed.size(); i++)
+      {
+         if (flowerbed[i] == 0)
+         {
+            if ((i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0))
+            {
+               n--;
+               flowerbed[i] = 1;
+            }
+         }
+      }
+      return n <= 0;
+   }
+};
